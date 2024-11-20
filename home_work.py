b49d983aff8554107f6fee17d8ac202929ae8328
elvyud11na <elvyud11na@gmail.com>
2024-11-20 10:27:06 +0300

    Первый коммит

---
+++ home_work.py
@@ -0,0 +1,23 @@
+import os
+
+import requests
+
+response = requests.get("https://favqs.com/api/qotd")
+
+
+print(f"The Quote of the day: {response.json()['quote']['body']}")
+
+
+print(os.getenv("STAGE"), os.getenv("BROWSER"))
+# если что то изменили в образе не забываем его пересобрать\
+# docker build -f Dockerfile-api -t home_work:v3 .
+
+# если переменную передаем через терминал
+#  в терминале пишем docker run -e "STAGE=qa" -e "BROWSER=chrome" home_work:v3
+
+# если переменную считываем из .env файла то в терминале пишем
+# docker run --env-file=.env home_work:v3
+
+#Управляемый запуск
+#Запуск с выполнением команды в контейнере, например когда в образе нет инструкции CMD.
+ # docker run home_work:v3 sh -c "python3 home_work.py"
